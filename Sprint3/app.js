e55dd60765b2574fa289fe609dbd03d35e6670ec
EduardoMiyasaki <eduardomiyasaki22@gmail.com>
2024-06-04 10:55:19 -0300

    Cadastrar caminhao Completo

--- Sprint3/app.js
+++ Sprint3/app.js
@@ -22,6 +22,7 @@ var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
 var bobRouter = require("./src/routes/bob");
+var CaminhaoRouter = require("./src/routes/Caminhao");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,6 +37,7 @@ app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
 app.use("/bob", bobRouter);
+app.use("/Caminhao" , CaminhaoRouter);
 
 
 app.listen(PORTA_APP, function () {
--- Sprint3/src/routes/Caminhao.js
+++ Sprint3/src/routes/Caminhao.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var CaminhaoController = require("../controllers/CaminhaoController");
 
 //Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
-router.post("/cadastrar", function (req, res) {
+router.post("/cadastrarCaminhao", function (req, res) {
     CaminhaoController.cadastrarCaminhao(req, res);
 });
 
