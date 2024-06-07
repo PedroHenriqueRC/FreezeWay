32db86562119bac630e762a75b23b1beb7ea3570
Fabio.TH <fabio.azevedo@sptech.school>
2024-06-06 22:08:15 -0300

    Adição da função autenticar N3, Models, routes e controllers

--- Sprint3/src/controllers/usuarioController.js
+++ Sprint3/src/controllers/usuarioController.js
@@ -5,9 +5,9 @@ function autenticar(req, res) {
     var email_empresaVar = req.body.emailServer;
     var primeira_senhaVar = req.body.senhaServer;
 
-    if (email_empresaVar == undefined) {
+    if (email_empresaVar === undefined) {
         res.status(400).send("Seu email está undefined!");
-    } else if (primeira_senhaVar == undefined) {
+    } else if (primeira_senhaVar === undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
         usuarioModel.autenticar(email_empresaVar, primeira_senhaVar)
@@ -15,14 +15,51 @@ function autenticar(req, res) {
                 console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                 console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
-                if (resultadoAutenticar.length == 1) {
+                if (resultadoAutenticar.length === 1) {
                     console.log(resultadoAutenticar);
                     res.json({
                         idUsuario: resultadoAutenticar[0].id,
                         nome: resultadoAutenticar[0].nome,
                         email: resultadoAutenticar[0].email
                     });
-                } else if (resultadoAutenticar.length == 0) {
+                } else if (resultadoAutenticar.length === 0) {
+                    res.status(403).send("Email e/ou senha inválido(s)");
+                } else {
+                    res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                }
+            }).catch(function (erro) {
+                console.log(erro);
+                console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            });
+    }
+}
+
+function autenticarN3(req, res) {
+    var idUsuario = req.body.idUsuarioServer;
+    var email = req.body.emailServer;
+    var Senha = req.body.senhaServer;
+
+    if (email === undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (Senha === undefined) {
+        res.status(400).send("Sua senha está indefinida!");
+    } else if (idUsuario === undefined) {
+        res.status(400).send("Seu ID de usuário está undefined!");
+    } else {
+        usuarioModel.autenticarN3(idUsuario, email, Senha)
+            .then(function (resultadoAutenticar) {
+                console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
+
+                if (resultadoAutenticar.length === 1) {
+                    console.log(resultadoAutenticar);
+                    res.json({
+                        idUsuario: resultadoAutenticar[0].id,
+                        nome: resultadoAutenticar[0].nome,
+                        email: resultadoAutenticar[0].email
+                    });
+                } else if (resultadoAutenticar.length === 0) {
                     res.status(403).send("Email e/ou senha inválido(s)");
                 } else {
                     res.status(403).send("Mais de um usuário com o mesmo login e senha!");
@@ -41,13 +78,13 @@ function cadastrar(req, res) {
     var email_empresaVar = req.body.email_empresaServer;
     var primeira_senhaVar = req.body.primeira_senhaServer;
 
-    if (nome_empresaVar == undefined) {
+    if (nome_empresaVar === undefined) {
         res.status(400).send("Nome da empresa está undefined!");
-    } else if (cnpj_empresaVar == undefined) {
+    } else if (cnpj_empresaVar === undefined) {
         res.status(400).send("CNPJ da empresa está undefined!");
-    } else if (email_empresaVar == undefined) {
+    } else if (email_empresaVar === undefined) {
         res.status(400).send("Email da empresa está undefined!");
-    } else if (primeira_senhaVar == undefined) {
+    } else if (primeira_senhaVar === undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else {
         usuarioModel.cadastrar(nome_empresaVar, cnpj_empresaVar, email_empresaVar, primeira_senhaVar)
@@ -63,5 +100,6 @@ function cadastrar(req, res) {
 
 module.exports = {
     autenticar,
+    autenticarN3,
     cadastrar
 }
--- Sprint3/src/routes/usuarios.js
+++ Sprint3/src/routes/usuarios.js
@@ -12,4 +12,8 @@ router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
 });
 
+router.post("/autenticarN3", function (req, res) {
+    usuarioController.autenticar(req, res);
+});
+
 module.exports = router;
